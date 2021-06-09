b9a97df8f316d17c947b60f0dbdc68c0e348eb6f
PeretTuY <xiaochun@gzsunrun.cn>
2021-06-09 11:27:47 +0800

    Say something

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel  Xcclwy 
                 </div>
 
                 <div class="links">
